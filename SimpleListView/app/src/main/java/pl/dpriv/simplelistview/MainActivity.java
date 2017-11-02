b684019946d5baf9ef8a31b6999c5f6a2a128439
pawel8273 <pawel@dpriv.pl>
2017-11-02 19:08:22 +0100

    Add menu option

--- SimpleListView/app/src/main/java/pl/dpriv/simplelistview/MainActivity.java
+++ SimpleListView/app/src/main/java/pl/dpriv/simplelistview/MainActivity.java
@@ -2,6 +2,8 @@ package pl.dpriv.simplelistview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -54,5 +56,12 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         Toast.makeText(this, name, Toast.LENGTH_SHORT).show();
 
         view.animate().rotationX(360).setDuration(2000).start();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.option_menu, menu);
+        return true;
     }
 }
